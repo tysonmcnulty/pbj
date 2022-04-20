ea9de1b760b6662bd692d3b97cc5ed8418a65d0d
Tyson McNulty <mcnultyt@vmware.com>
2022-04-20 09:52:43 -0400

    Initial commit -- root Gradle project and  subproject

---
+++ feature/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.6.21"
+    `java-library`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation(kotlin("test"))
+}
